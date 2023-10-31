4f373c10ca2c0b5ac292ecba984d81b6e0d30ebc
Jordan Wamser <jmwamser@me.com>
2023-10-30 20:20:13 -0500

    add toasts using stimulus

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -17,6 +17,7 @@ use App\Repository\EventRepository;
 use App\Service\Mailer\RegistrationLeaderNotificationContextAwareMailer;
 use App\Service\Mailer\RegistrationThankYouContextAwareMailer;
 use App\Service\PersonManager;
+use DateTime;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mime\Address;
@@ -79,6 +80,11 @@ class RegistrationController extends AbstractController
     #[Route('/register/{event}/server', name: 'app_registration_server_formentry')]
     public function serverRegistration(Event $event,Request $request)
     {
+        if (new DateTime() > $event->getRegistrationDeadLineServers() && $this->getGlobalSettings()->isRegistrationDeadlineInforced()) {
+            $this->addFlash('error',"The deadline for server registration has passed.");
+            return $this->redirectToRoute('app_registration_list');
+        }
+
         $eventRegistration = new EventParticipant();
         $eventRegistration->setEvent($event);
         $form = $this->createForm(ServerEventParticipantType::class,$eventRegistration);
