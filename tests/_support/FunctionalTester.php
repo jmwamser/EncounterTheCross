f4cf092e8d6f57a4107daf761396488c6eabd8d5
Jordan Wamser <jmwamser@me.com>
2023-12-15 17:37:04 -0600

    add test configuration

--- tests/_support/FunctionalTester.php
+++ tests/_support/FunctionalTester.php
@@ -27,26 +27,34 @@ class FunctionalTester extends \Codeception\Actor
 {
     use _generated\FunctionalTesterActions;
 
-    #[Given('I am logged in as a Leader')]
+    /**
+     * @Given /^I am logged in as a Leader$/
+     */
     public function iAmLoggedInAsALeader()
     {
         throw new IncompleteTestError();
     }
 
-    #[Given('I am on the "<arg1>" List Page')]
+    /**
+     * @Then /^I should not see a "([^"]*)" action$/
+     */
+    public function iShouldNotSeeAAction($arg1)
+    {
+        throw new IncompleteTestError();
+    }
+
+    /**
+     * @Given /^I am on the "([^"]*)" List Page$/
+     */
     public function iAmOnTheListPage($arg1)
     {
         throw new IncompleteTestError();
     }
 
-    #[When('I click on the action menu for an "<arg1>"')]
+    /**
+     * @When /^I click on the action menu for an "([^"]*)"$/
+     */
     public function iClickOnTheActionMenuForAn($arg1)
-    {
-        throw new IncompleteTestError();
-    }
-
-    #[Then('I should not see a "<arg1>" action')]
-    public function iShouldNotSeeAAction($arg1)
     {
         throw new IncompleteTestError();
     }
